a0bb7af2732d2bce4fa8afa82a9cc604f7ba6d29
sandra <smmoninog01@iesalbarregas.es>
2026-03-02 19:26:59 +0100

    Eliminar iconos temporales

--- lib/componentes/bottomNav.dart
+++ lib/componentes/bottomNav.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:kultux/componentes/iconosBottom.dart';
 class BottomNav extends StatelessWidget{
   const BottomNav({super.key});
   @override
